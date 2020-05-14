404acd7b814a26eadba592a40e9cd48f62f71a00
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-05-14 13:01:22 +0300

    Рабочая версия программы.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+public class Example
+{
+    public static void Main()
+    {
+
+        int key = 0xACACAC; //ключ шифрования нужно выбирать большим за данные которые шифруются
+        int password = 123456789; //важные данные которые необходимо зашифровать
+
+        int encryptedPass = password ^ key;
+        Console.WriteLine("Зашифрованный пароль: {0}", encryptedPass);
+
+        int decryptedPass = encryptedPass ^ key;
+        Console.WriteLine("Расшифрованный пароль: {0}", decryptedPass);
+
+
+        string strings = "Декодер преобразует массив байтов, отражающий конкретную кодировку символов, в набор символов в массиве символов или в строке. Чтобы декодировать массив байтов в массив символов, вызовите метод Encoding.GetChars . Чтобы декодировать массив байтов в строку, вызовите метод GetString . Если перед декодированием нужно определить, сколько символов требуется для хранения раскодированных байтов, можно вызвать метод GetCharCount .";
+        Encoding asciiEncoding = Encoding.Default;
+
+        // Create array of adequate size.
+        byte[] bytes = new byte[strings.Length];
+        // Create index for current position of array.
+        int index = 0;
+
+        Console.WriteLine("Strings to encode:");
+
+        Console.WriteLine("   {0}", strings);
+
+        asciiEncoding.GetBytes(strings, 0, strings.Length, bytes, index);
+        Console.WriteLine("\nEncoded bytes:");
+        Console.WriteLine("{0}", ShowByteValues(bytes, bytes.Length));
+        Console.WriteLine();
+
+        // Decode Unicode byte array to a string.
+        string newString = asciiEncoding.GetString(bytes, 0, bytes.Length);
+        Console.WriteLine("Decoded: {0}", newString);
+        Console.ReadKey();
+    }
+
+    private static string ShowByteValues(byte[] bytes, int last)
+    {
+        string returnString = "   ";
+        for (int ctr = 0; ctr <= last - 1; ctr++)
+        {
+            if (ctr % 50 == 0)
+                returnString += "\n   ";
+            returnString += String.Format("{0:X} ", bytes[ctr]);
+        }
+        return returnString;
+    }
+}
+// The example displays the following output:
+//       Strings to encode:
+//          This is the first sentence.
+//          This is the second sentence.
+//
+//       Encoded bytes:
+//
+//          54 68 69 73 20 69 73 20 74 68 65 20 66 69 72 73 74 20 73 65
+//          6E 74 65 6E 63 65 2E 20 54 68 69 73 20 69 73 20 74 68 65 20
+//          73 65 63 6F 6E 64 20 73 65 6E 74 65 6E 63 65 2E 20
+//
+//       Decoded: This is the first sentence. This is the second sentence.
+
--- Lesson2/Additional Task/Program.cs
+++ Lesson2/Additional Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,66 @@ using System.Threading.Tasks;
 
 namespace Additional_Task
 {
+    /*Используя класс SortedList, создайте небольшую коллекцию и выведите на экран значения пар
+«ключ- значение» сначала в алфавитном порядке, а затем в обратном.*/
+    struct Human                // Объявление класса Human
+    {
+        public string Name { get; }    // Открытое автосвойство только для чтения, определяющее имя человека
+        public string Surname { get; }  // Открытое автосвойство только для чтения, определяющее фамилию человека
+        public Human(string name, string surname)  // Пользовательский конструктор для инициализации полей автосвойств класса Human
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+    }
+    public class DescendingCompare : IComparer   // Открытый класс, реализует возможность обратной сортировки набора данных о людях
+    {
+        CaseInsensitiveComparer comparer = new CaseInsensitiveComparer(); // Создаем экземпляр класса, который проверяет равенство двух объектов без учета регистра строк.
+
+        /// <summary>
+        /// Сравнивает два объекта и возвращает значение, показывающее, что один объект меньше или больше другого или равен ему.
+        /// </summary>
+        /// <param name="x">Первый сравниваемый объект.</param>
+        /// <param name="y">Второй сравниваемый объект.</param>
+        /// <returns>Знаковое целое число, которое определяет относительные значения x и y, как показано в следующей таблице.Значение Значение Меньше нуля Значение параметра x меньше значения параметра y. Zero Значение параметра x равно значению параметра y. Больше нуля. Значение x больше значения y.</returns>
+        public int Compare(object x, object y)  
+        {
+            int result = comparer.Compare(y, x); // Порядок расположения аргументов в методе Compare: y,x - сортировка по убыванию; x,y - сортировка по возрастанию
+            return result;
+        }
+    }
+
     class Program
     {
+        
         static void Main(string[] args)
         {
+            SortedList sortedList = new SortedList() // Создаем экземпляр класса SortedList. Предоставляет коллекцию пар "ключ-значение", упорядоченных по ключам. Доступ
+                                                     //     к парам можно получить по ключу и по индексу.
+            {
+                { new Human("Sergey", "Grigoriev").Surname, new Human("Sergey", "Grigoriev").Name },  // Добавление элементов типа Human в блок инициализатора конструктора класса
+                { new Human("Dmitro", "Vasilev").Surname, new Human("Dmitro", "Vasilev").Name },
+                { new Human("Elena", "Yashuk").Surname, new Human("Elena", "Yashuk").Name },
+                { new Human("Oleg", "Hluhov").Surname, new Human("Oleg", "Hluhov").Name },
+                { new Human("Andrey", "Ilin").Surname, new Human("Andrey", "Ilin").Name },
+                { new Human("Roman", "Trunov").Surname, new Human("Roman", "Trunov").Name },
+            };
+            sortedList.Add(new Human("Piter", "Vasnetsov").Surname, new Human("Piter", "Vasnetsov").Name);  // Добавление элемента в коллекцию, используя метод Add
+            sortedList[new Human("Piter", "Vasnetsov").Surname] = "Igor";  // Изменение значения по ключу коллекции, используя индексатор
+            foreach (DictionaryEntry item in sortedList)  // Перебор коллекции пар (ключ-значение) используя оператор foreach. Поочередно извлекая из коллекции sortedList элементы и присваивает значение переменной итерации item типа DictionatyEntry
+            {    // Структура DictionaryEntry - определяет пару ключ/значение словаря, которая может быть задана или получена.
+                Console.WriteLine(item.Key+ "    \t   "+ item.Value);
+            }
+            Console.WriteLine(new string('-', 30));
+
+            // Создаем новый экземпляр коллекции типа SortedList, вызов конструктора которого реализует возможность обратной сортировки элементов по ключу ранее созданной нами коллекции
+            SortedList descendingSortedList = new SortedList(sortedList, new DescendingCompare());
+            foreach (DictionaryEntry item in descendingSortedList) // Перебор всех элементов коллекции и вывод в консоли
+            {
+                Console.WriteLine(item.Key + "    \t   " + item.Value);
+            }
+            Console.WriteLine(new string('-', 30));
+            Console.ReadKey();
         }
     }
 }

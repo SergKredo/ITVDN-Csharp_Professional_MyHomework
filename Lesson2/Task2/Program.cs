9f8fd5a17629f6ed365d8dba675ff895080a4344
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-05-14 18:15:33 +0300

    Task2. Создание проекта

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+        /*Задание 2
+    Создайте коллекцию, в которую можно добавлять покупателей и категорию приобретенной ими
+    продукции. Из коллекции можно получать категории товаров, которые купил покупатель или по
+    категории определить покупателей.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

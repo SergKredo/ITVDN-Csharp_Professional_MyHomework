6d3fc6b51b06944d68894df6c6f21579088791bc
sergiokredo <sergeykredenser@gmail.com>
2020-05-16 00:47:52 +0300

    Исправлены ошибки в задании №2

--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Additional_Task;
 
 namespace Task2
 {
@@ -16,32 +17,38 @@ namespace Task2
     {
         public string Name { get; }
         public string Surname { get; }
-        public Customer(string name, string surname): this()
+        public Customer(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
-        
+
         public override string ToString()
         {
             return string.Format(Name + " " + Surname);
         }
     }
-    public class CustomerCollection: SortedList
+    public class CustomerCollection : SortedList
     {
-        DescendingCompare compare = new DescendingCompare();
         Customer customer;
-        public CustomerCollection(DescendingCompare compare)
+        public CustomerCollection()
         {
 
         }
+        public CustomerCollection(CustomerCollection list, IComparer compare) : base(list, compare)
+        {
+            foreach (DictionaryEntry item in list)
+            {
+                this.Add(item.Key as string, ((Customer)item.Value).Name, ((Customer)item.Value).Surname);
+            }
+        }
+
         public void Add(string product, string name, string surname)
         {
             customer = new Customer(name, surname);
             this[product] = customer;
         }
 
-        
         public string this[string name, string surname]
         {
             get
@@ -57,28 +64,12 @@ namespace Task2
             }
         }
     }
-    public class DescendingCompare: IComparer  // Открытый класс, реализует возможность обратной сортировки набора данных о людях
-    {
-        CaseInsensitiveComparer comparer = new CaseInsensitiveComparer(); // Создаем экземпляр класса, который проверяет равенство двух объектов без учета регистра строк.
-
-        /// <summary>
-        /// Сравнивает два объекта и возвращает значение, показывающее, что один объект меньше или больше другого или равен ему.
-        /// </summary>
-        /// <param name="x">Первый сравниваемый объект.</param>
-        /// <param name="y">Второй сравниваемый объект.</param>
-        /// <returns>Знаковое целое число, которое определяет относительные значения x и y, как показано в следующей таблице.Значение Значение Меньше нуля Значение параметра x меньше значения параметра y. Zero Значение параметра x равно значению параметра y. Больше нуля. Значение x больше значения y.</returns>
-        public int Compare(object x, object y)
-        {
-            int result = comparer.Compare(x, y); // Порядок расположения аргументов в методе Compare: y,x - сортировка по убыванию; x,y - сортировка по возрастанию
-            return result;
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            CustomerCollection collection = new CustomerCollection(new DescendingCompare())
+            CustomerCollection collection = new CustomerCollection()
             {
                 {"smartphone", "Andrey", "Ivanov"}, {"laptop", "Elena","Lapina"}, {"dishwasher", "Inna", "Trofimova"}
             };
@@ -94,6 +85,12 @@ namespace Task2
             Console.WriteLine(new string('-', 20));
             Console.WriteLine(collection["hoover"]);
             Console.WriteLine(collection["Pavel", "Vorovtsev"]);
+            Console.WriteLine(new string('-', 20));
+            CustomerCollection collectionTwo = new CustomerCollection(collection, new DescendingCompare());
+            foreach (DictionaryEntry item in collectionTwo)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
             Console.ReadKey();
         }
     }

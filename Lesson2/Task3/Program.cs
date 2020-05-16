276a9997997393f773ee0bf846da93dd0eae5fcc
sergiokredo <sergeykredenser@gmail.com>
2020-05-16 13:21:52 +0300

    Создание нового проекта.Задание №3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+        /*Задание 3
+    Несколькими способами создайте коллекцию, в которой можно хранить только целочисленные и
+    вещественные значения, по типу «счет предприятия – доступная сумма» соответственно.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

c57cf69935dcbd04589fcde599e45269dd13492b
sergiokredo <sergeykredenser@gmail.com>
2020-05-17 11:37:13 +0300

    Создание нового проекта

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+        /*Задание 4
+    Создайте коллекцию типа OrderedDictionary и реализуйте в ней возможность сравнения значений
+    ключей.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}

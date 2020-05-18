02bf6a821bd2abad47b8cdd36021ef80087fc7f1
sergiokredo <sergeykredenser@gmail.com>
2020-05-18 13:13:03 +0300

    Рабочая версия задания №4 с комментариями.

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -12,30 +12,41 @@ namespace Task4
 Создайте коллекцию типа OrderedDictionary и реализуйте в ней возможность сравнения значений
 ключей.*/
 
-    public class EqualityComparer : IEqualityComparer
+    public class EqualityComparer : IEqualityComparer  // Интерфейс IEqualityComparer определяет методы для поддержки операций сравнения объектов в отношении равенства.
     {
-        bool System.Collections.IEqualityComparer.Equals(object oneKey, object twoKey)
+        CaseInsensitiveComparer instance = new CaseInsensitiveComparer(); // Создаем экземпляр класса CaseInsensitiveComparer. Проверяет равенство двух объектов без учета регистра строк.
+        public new bool Equals(object oneKey, object twoKey)  //Метод, который определяет, равны ли два указанных объекта. True, если указанные объекты равны; в противном случае — false.
         {
-            return true;
+            bool i = instance.Compare(oneKey, twoKey) == 0;
+            return i;
         }
-        int System.Collections.IEqualityComparer.GetHashCode(object obj)
+        public int GetHashCode(object obj)   // Метод, который возвращает хэш-код указанного объекта.
         {
-            return 0;
+            int i = obj.ToString().ToLowerInvariant().GetHashCode();  // Хеш-код для копии данного объекта переведенного в нижний регистр
+            return i;
         }
     }
-    public class MyCollection : OrderedDictionary
+    public class MyCollection : OrderedDictionary  // Создаем пользовательскую коллекцию, которая наследуется от системного класса OrderedDictionary
     {
+        EqualityComparer compare;
         string Name { get; set; }
         int Age { get; set; }
 
         public MyCollection(EqualityComparer compare)
         {
-
+            this.compare = compare;
         }
-        public new void Add(object key, object value)
+        public new void Add(object key, object value)   // Метод Add, который позволяет добавить новый элемент пары(ключ-значение) в коллекцию.
         {
             this.Name = (string)key;
             this.Age = (int)value;
+            foreach (DictionaryEntry item in this)   // Проверка на равество элементов с данным ключем и значением в коллекции
+            {
+                if (item.Key.ToString().ToLowerInvariant().GetHashCode() == this.compare.GetHashCode(Name) && (int)item.Value == this.Age)
+                {
+                    return;  // Если такой ключ и значение уже есть в коллекции, то выходим из метода
+                }
+            }
             this[Name] = Age;
         }
     }
@@ -44,15 +55,33 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            MyCollection collection = new MyCollection(new EqualityComparer());
+            MyCollection collection = new MyCollection(new EqualityComparer())
+            {
+                {"Ivan", 16}, {"Egor", 36}, {"Maria", 19}, {"Tatiana", 22}
+            };
             collection.Add("Piter", 18);
-            collection.Add("piter", 22);
+            collection.Add("piter", 34);
+            collection.Add("Piter", 18);
+            collection.Add("Piter", 34);
+            collection.Add("PitEr", 42);
+            collection["Piter"] = 18;
             foreach (DictionaryEntry item in collection)
             {
-                Console.WriteLine(item.Key + ": "+ item.Value);
-            }
-
-
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }   
         }
     }
 }
+
+/* 
+Result:
+***********************************************************************
+Ivan: 16
+Egor: 36
+Maria: 19
+Tatiana: 22
+Piter: 18
+piter: 34
+PitEr: 42
+***********************************************************************
+*/

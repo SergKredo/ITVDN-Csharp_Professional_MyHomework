f8e3b290110481cb3bfa95be665141278c7c27a4
sergiokredo <sergeykredenser@gmail.com>
2020-05-22 20:33:00 +0300

    Выполнение задания №3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+        /*Задание 3
+    Напишите приложение для поиска заданного файла на диске. Добавьте код, использующий
+    класс FileStream и позволяющий просматривать файл в текстовом окне. В заключение
+    добавьте возможность сжатия найденного файла.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

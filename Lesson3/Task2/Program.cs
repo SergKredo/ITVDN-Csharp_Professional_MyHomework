e35cfce614b1de1ca7b113a65f068d3b8d9a3c5a
sergiokredo <sergeykredenser@gmail.com>
2020-05-22 15:54:07 +0300

    Выполнение 2-го задания.

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+        /*Задание 2
+    Создайте файл, запишите в него произвольные данные и закройте файл. Затем снова откройте
+    этот файл, прочитайте из него данные и выведете их на консоль.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

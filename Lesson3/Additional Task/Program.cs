b2ff8f30cbb0a7630be1dc6aeacb76d8771a9fc5
sergiokredo <sergeykredenser@gmail.com>
2020-05-22 14:38:27 +0300

    Выполнение задания №1. Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.

--- Lesson3/Additional Task/Program.cs
+++ Lesson3/Additional Task/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Additional_Task
 {
+    /*Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.*/
     class Program
     {
         static void Main(string[] args)

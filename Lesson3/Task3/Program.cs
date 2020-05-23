9a100440161bb96a6f14c53386245729b6bcca39
sergiokredo <sergeykredenser@gmail.com>
2020-05-23 18:51:28 +0300

    Поисковик  с исправлениями

--- Lesson3/Task3/Program.cs
+++ Lesson3/Task3/Program.cs
@@ -20,11 +20,26 @@ namespace Task3
         static void Main(string[] args)
         {
 
+            TimeSpan time;
+            DateTime timeINStart, timeIN, timeOut;
+            string fullName = null;
             DriveInfo[] drivers = DriveInfo.GetDrives();
+            timeINStart = DateTime.Now;
+            timeIN = DateTime.Now;
+
             foreach (var item in drivers)
             {
                 DirectoryInfo searchFiels = new DirectoryInfo(@item.RootDirectory.FullName);
-                FileSystemInfo[] filesOne = searchFiels.GetDirectories();
+                FileInfo[] filesThour = searchFiels.GetFiles("Proton.txt", SearchOption.TopDirectoryOnly);
+                if (filesThour.Length != 0)
+                {
+                    timeOut = DateTime.Now;
+                    time = timeOut - timeIN;
+                    timeIN = timeOut;
+                    Console.WriteLine(filesThour[0].FullName + " " + "Time = {0} sec", time.Seconds);
+                }
+
+                DirectoryInfo[] filesOne = searchFiels.GetDirectories();
 
                 foreach (var items in filesOne)
                 {
@@ -32,23 +47,100 @@ namespace Task3
                     {
                         string nameDirectory = items.FullName;
                         DirectoryInfo searchOne = new DirectoryInfo(@nameDirectory);
-                        FileInfo[] files = searchOne.GetFiles("Proton.txt", SearchOption.AllDirectories);
-                        if (files.Length != 0)
+                        DirectoryInfo[] filesTwo = searchOne.GetDirectories();
+                        foreach (var itemTwo in filesTwo)
                         {
-                            Console.WriteLine(files[0].FullName);
+                            try
+                            {
+                                string nameDirectoryh = itemTwo.FullName;
+                                DirectoryInfo searchOneh = new DirectoryInfo(nameDirectoryh);
+                                var filesFive = searchOneh.GetDirectories();
+                                foreach (var itemFive in filesFive)
+                                {
+                                    try
+                                    {
+                                        FileInfo[] files = itemFive.GetFiles("Proton.txt", SearchOption.AllDirectories);
+                                        if (files.Length != 0)
+                                        {
+                                            timeOut = DateTime.Now;
+                                            time = timeOut - timeIN;
+                                            timeIN = timeOut;
+                                            fullName = files[0].FullName;
+                                            Console.WriteLine(files[0].FullName + " " + "Time = {0} sec", time.Seconds);
+                                        }
+                                    }
+                                    catch (Exception) { }
+
+                                    string nameDirectoryss = itemFive.FullName;
+                                    DirectoryInfo searchOness = new DirectoryInfo(@nameDirectoryss);
+                                    try
+                                    {
+                                        FileInfo[] filesTHreesss = searchOness.GetFiles("Proton.txt", SearchOption.TopDirectoryOnly);
+                                        if (filesTHreesss.Length != 0)
+                                        {
+                                            if (fullName == filesTHreesss[0].FullName)
+                                            {
+                                                continue;
+                                            }
+                                            timeOut = DateTime.Now;
+                                            time = timeOut - timeIN;
+                                            timeIN = timeOut;
+                                            Console.WriteLine(filesTHreesss[0].FullName + " " + "Time = {0} sec", time.Seconds);
+                                        }
+                                    }
+                                    catch (Exception)
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                            catch (Exception) { }
+
+                            string nameDirectorysss = itemTwo.FullName;
+                            DirectoryInfo searchOnesss = new DirectoryInfo(@nameDirectorysss);
+                            try
+                            {
+                                FileInfo[] filesTHreessss = searchOnesss.GetFiles("Proton.txt", SearchOption.TopDirectoryOnly);
+                                if (filesTHreessss.Length != 0)
+                                {
+                                    timeOut = DateTime.Now;
+                                    time = timeOut - timeIN;
+                                    timeIN = timeOut;
+                                    Console.WriteLine(filesTHreessss[0].FullName + " " + "Time = {0} sec", time.Seconds);
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
                         }
                     }
-                    catch (Exception e)
+                    catch (Exception) { }
+                    
+                    string nameDirectorys = items.FullName;
+                    DirectoryInfo searchOnes = new DirectoryInfo(@nameDirectorys);
+                    try
+                    {
+                        FileInfo[] filesTHreesh = searchOnes.GetFiles("Proton.txt", SearchOption.TopDirectoryOnly);
+                        if (filesTHreesh.Length != 0)
+                        {
+                            timeOut = DateTime.Now;
+                            time = timeOut - timeIN;
+                            timeIN = timeOut;
+                            Console.WriteLine(filesTHreesh[0].FullName + " " + "Time = {0} sec", time.Seconds);
+                        }
+                    }
+                    catch (Exception)
                     {
                         continue;
                     }
                 }
-                filesOne = searchFiels.GetFiles("Proton.txt", SearchOption.TopDirectoryOnly);
-                if (filesOne.Length != 0)
-                {
-                    Console.WriteLine(filesOne[0].FullName);
-                }
             }
+
+           
+            timeOut = DateTime.Now;
+            time = timeOut - timeINStart;
+            Console.WriteLine("Total time = {0} sec", time.Seconds);
             Console.ReadKey();
         }
     }

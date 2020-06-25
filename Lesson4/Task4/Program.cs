8184ca2ab3f9ad24c42bf0393ba249ac3013b97c
sergiokredo <sergeykredenser@gmail.com>
2020-06-25 23:46:43 +0300

    Задание №4

---
+++ Lesson4/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+        /*Задание 4
+    Создайте текстовый файл-чек по типу «Наименование товара – 0.00 (цена) грн.» с
+    определенным количеством наименований товаров и датой совершения покупки. Выведите на
+    экран информацию из чека в формате текущей локали пользователя и в формате локали en-
+    US.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

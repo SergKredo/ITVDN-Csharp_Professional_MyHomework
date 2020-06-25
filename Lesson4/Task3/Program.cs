8407d41d1c77b53f2bfd62c3d2c2d40d88c8a3a1
sergiokredo <sergeykredenser@gmail.com>
2020-06-25 23:43:38 +0300

    Рабочая версия программы без комментариев!

--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -8,6 +8,9 @@ using System.IO;
 
 namespace Task3
 {
+        /*Задание 3
+    Напишите шуточную программу «Дешифратор», которая бы в текстовом файле могла бы
+    заменить все предлоги на слово «ГАВ!».*/
     class Program
     {
         static void Main(string[] args)
@@ -15,27 +18,16 @@ namespace Task3
             Stream stream = File.OpenRead(@"Prepositions.txt");
             StreamReader streamReader = new StreamReader(stream);
             string prepositions = streamReader.ReadToEnd();
-            Console.WriteLine(prepositions);
-            Console.WriteLine(new string('-', 100));
             string pattern = @"\s?\(\S*\s?\S*\)\s";
             prepositions = Regex.Replace(prepositions, pattern, "");
-            Console.WriteLine(prepositions);
-            Console.WriteLine(new string('-', 100));
-            Console.WriteLine(new string('-', 100));
+            
             pattern = @"\,";
             prepositions = Regex.Replace(prepositions, pattern, "\n");
-            Console.WriteLine(prepositions);
-            Console.WriteLine(new string('-', 100));
-            Console.WriteLine(new string('-', 100));
-
+            
             prepositions = prepositions.ToLowerInvariant();
             pattern = @"\d";
             prepositions = Regex.Replace(prepositions, pattern, "");
-            Console.WriteLine(prepositions);
-
-            Console.WriteLine(new string('-', 100));
-            Console.WriteLine(new string('-', 100));
-
+            
             List<string> words = new List<string>();
             pattern = @"(?<words>\W?\S*\ *\S*\ *\S*\ *\S*)";
             Regex regex = new Regex(pattern);
@@ -46,49 +38,68 @@ namespace Task3
                 newText += "\"" + item.Groups["words"].Value + "\"" + ",";
             }
             words.RemoveAt(words.Count - 1);
-            Console.WriteLine(newText);
-
+            
             StreamWriter streamWriter = new StreamWriter(@"Prepositions_redact.txt");
             streamWriter.Write(newText);
             streamWriter.Close();
 
-            string text = @"Поезд несется от Уральских гор мимо дремучих лесов,
-                            мимо домен и школ, через величайшую реку Европы – Волгу, к
-                            Москве с ее древним Кремлем, с ее молодостью века и дальше,
-                            мимо воскресшей из пепла Варшавы, мимо двуликого Берлина,
-                            через Рейн с его замками, к Парижу, похожему на каменную
-                            рощу столетий, и еще дальше, до последнего мыса Европы, где
-                            под суровыми океанскими ветрами бретонский рыбак сушит
-                            свои сети(И.Эренбург). 2. К вечеру мы немного не дошли до
-                            реки Черниговки и стали биваком на узком перешейке между
-                            ней и небольшой протокой.Ширина реки здесь колеблется от
-                            15 до 80 метров. При этом она отделяет от себя в сторону большие 
-                            слепые рукава, от которых идут длинные, узкие и глубокие
-                            канавы, сообщающиеся с озерами и болотами или с такими речками,
-                            которые впадают в Лефу(В.Арсеньев). 3. Выйдя из сарая,
-                            я увидел в небе борьбу света с темными облаками, и впечатление
-                            оставалось такое, что солнце победит(М.Пришвин).
-                            4. Из 1925 году появился ныне всемирно известный большой
-                            проекционный аппарат – планетарий Цейси. Его изобретение
-                            связано с именем инженера В.Бауерсфельда.Аппарат может
-                            работать в зале с куполом от 18 до 30 метров в диаметре.";
+            stream = File.OpenRead(@"Tolstoyi_L._Detstvootroche3._Yunost.txt");
+            streamReader = new StreamReader(stream);
+            string text = streamReader.ReadToEnd();
+            stream.Close();
+            streamReader.Close();
+            
+
             Console.WriteLine(text);
             Console.WriteLine(new string('-', 100));
+            Console.WriteLine(new string('-', 100));
+
             string patter = null;
             foreach (string item in words)
             {
                 patter += @"(?<prepositions>\W{1}" + item + @"\s)|";
             }
             patter = patter.Substring(0, patter.Length - 1);
-            //Console.WriteLine(patter);
+
+            List<string> prepor = new List<string>();
             Regex regext = new Regex(patter);
             foreach (Match items in regext.Matches(text))
             {
-                Console.WriteLine("Preposition: {0}", items.Groups["prepositions"].Value);
+                prepor.Add(items.Groups["prepositions"].Value);
             }
-            //patter = @"(?<prepositions>\W{1}" + @"из" + @"\s)";
-            text = Regex.Replace(text.ToLowerInvariant(), patter, " ГАВ ");
+            text = Regex.Replace(text, patter, " <Квитонька> ");
+
+            words = new List<string>();
+            foreach (Match item in regex.Matches(prepositions))
+            {
+                string word = ((item.Groups["words"].Value).Replace("\n", "") == "")? " ": (item.Groups["words"].Value).Replace("\n", "");
+                words.Add(word.Replace(word[0].ToString(), word[0].ToString().ToUpper()));
+            }
+            words.RemoveAt(words.Count - 1);
+            patter = null;
+            foreach (string item in words)
+            {
+                patter += @"(?<prepositions>\W{1}" + item + @"\s)|";
+            }
+            patter = patter.Substring(0, patter.Length - 1);
+            regext = new Regex(patter);
+            foreach (Match items in regext.Matches(text))
+            {
+                prepor.Add(items.Groups["prepositions"].Value);
+            }
+            text = Regex.Replace(text, patter, " <Квитонька> ");
             Console.WriteLine(text);
+            Console.WriteLine(new string('-', 100));
+            Console.WriteLine(new string('-', 100));
+
+            foreach (string ite in prepor)
+            {
+                Console.Write(ite+", ");
+            }
+
+            streamWriter = new StreamWriter(@"Tolstoyi_L._Detstvootroche3._Yunost_redact.txt");
+            streamWriter.Write(text);
+            streamWriter.Close();
             Console.ReadKey();
 
         }

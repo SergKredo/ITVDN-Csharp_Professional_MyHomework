c866f3217eb69e53d0a3312a8dea2b32fed038b8
sergiokredo <sergeykredenser@gmail.com>
2020-08-01 20:34:58 +0300

    Рабочая версия программы

--- Lesson 6/Additional Task/Form1.cs
+++ Lesson 6/Additional Task/Form1.cs
@@ -7,14 +7,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
 
 namespace Additional_Task
 {
-    public partial class Form1 : Form
+    public static class Main
     {
-        public Form1()
+
+    }
+    public partial class TextBox : Form
+    {
+        public TextBox()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+
+                Assembly assembly = Assembly.LoadFrom(@path);
+                Type[] types = assembly.GetTypes();
+                string namespaces = null;
+
+                foreach (Type item in types)
+                {
+
+
+
+                    this.textBox1.Text += (namespaces == null) ? "namespace " + item.Namespace + Environment.NewLine + "{" + Environment.NewLine : null;
+                    namespaces = item.Namespace;
+                    if (item.IsClass)
+                    {
+                        this.textBox1.Text += "class ";
+                    }
+
+                    if (item.IsValueType)
+                    {
+                        this.textBox1.Text += "struct ";
+                    }
+
+                    if (item.IsEnum)
+                    {
+                        this.textBox1.Text += "enum ";
+                    }
+
+                    if (item.IsInterface)
+                    {
+                        this.textBox1.Text += "interface ";
+                    }
+                    this.textBox1.Text += item.Name + Environment.NewLine + "{" + Environment.NewLine;
+
+
+                    this.textBox1.Text += "Methods:".ToUpper() + Environment.NewLine;
+
+                    foreach (MethodInfo items in item.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.IgnoreCase))
+                    {
+                        if (items.IsPublic)
+                        {
+                            this.textBox1.Text += "public ";
+                        }
+
+                        if (items.IsPrivate)
+                        {
+                            this.textBox1.Text += "private ";
+                        }
+
+                        if (items.IsStatic)
+                        {
+                            this.textBox1.Text += "static ";
+                        }
+
+                        if (items.IsAbstract)
+                        {
+                            this.textBox1.Text += "abstract ";
+                        }
+
+                        if (items.IsVirtual)
+                        {
+                            this.textBox1.Text += "virtual ";
+                        }
+
+                        this.textBox1.Text += items.ReturnType.Name + " " + items.Name + Environment.NewLine;
+                    }
+
+                    this.textBox1.Text += "}" + Environment.NewLine + "\r\n";
+
+                }
+                this.textBox1.Text += "}" + Environment.NewLine;
+            }
+        }
+
+        private void TextBox_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

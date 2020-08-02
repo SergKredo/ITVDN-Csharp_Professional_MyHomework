a777c662eca5ee7907ba84ce0404210e9b0c3a1a
sergiokredo <sergeykredenser@gmail.com>
2020-08-02 19:34:01 +0300

    Приложение №2

---
+++ Lesson 6/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+
+    /*
+    Задание 2
+    Создайте свою пользовательскую сборку по примеру сборки CarLibrary из урока, сборка будет
+    использоваться для работы с конвертером температуры.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}

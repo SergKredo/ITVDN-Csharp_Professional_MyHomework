cdb7626e937ec48d3c0fe5aa121ba2c88afac606
sergiokredo <sergeykredenser@gmail.com>
2020-08-04 23:32:37 +0300

    Добавлена возможность ввода только цифр и точки с запятой.

--- Lesson 6/Task3_ConverterTemperature/Form1.cs
+++ Lesson 6/Task3_ConverterTemperature/Form1.cs
@@ -12,7 +12,13 @@ namespace Task3_ConverterTemperature
 {
     public partial class Form1 : Form
     {
-
+        char[] charMassive = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', ',' };
+        static double degreeCelcia = 0;
+        static double degreeKelvin = 0;
+        static double degreeFarinhate = 0;
+        static string textCelcia = "";
+        static string textKelvin = "";
+        static string textFarinhate = "";
         bool kelvin = true;
         bool farinhate = true;
         bool celcia = true;
@@ -29,72 +35,181 @@ namespace Task3_ConverterTemperature
         private void textBox_Kelvine_TextChanged(object sender, EventArgs e)
         {
             var text = sender as TextBox;
-            if (text.Text != "" && kelvin)
+            bool right = true;
+            
+            if (text.Text != "" && kelvin && !text.Text.StartsWith(".") && !text.Text.StartsWith(",") && (!textKelvin.Contains(",") || (textKelvin.IndexOf(',') == text.Text.LastIndexOf(','))) && (!textKelvin.Contains(".") || (textKelvin.IndexOf('.') == text.Text.LastIndexOf('.'))))
             {
                 if (text.Text != "-")
                 {
                     farinhate = celcia = false;
-                    double celciaDegree;
                     try
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace('.', ','));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeKelvin = Convert.ToDouble(text.Text.Replace('.', ','));
+                                textKelvin = text.Text;
+                                right = false;
+                            }
+                        }
                     }
                     catch (Exception)
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace(',', '.'));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeKelvin = Convert.ToDouble(text.Text.Replace(',', '.'));
+                                textKelvin = text.Text;
+                                right = false;
+                            }
+                        }
                     }
-                    this.textBox_Farinhate.Text = ConvertKelvineToFarinhate(celciaDegree).ToString();
-                    this.textBox_Celcia.Text = ConvertKelvineToCelcia(celciaDegree).ToString();
+                    if (right != false)
+                    {
+                        this.textBox_Kelvine.Text = degreeKelvin.ToString();
+                        textKelvin = degreeKelvin.ToString();
+                    }
+                    this.textBox_Farinhate.Text = ConvertKelvineToFarinhate(degreeKelvin).ToString();
+                    this.textBox_Celcia.Text = ConvertKelvineToCelcia(degreeKelvin).ToString();
                     farinhate = celcia = true;
                 }
+            }
+            else if (text.Text.StartsWith(",") || text.Text.StartsWith("."))
+            {
+                this.textBox_Kelvine.Text = degreeKelvin.ToString();
+                textKelvin = degreeKelvin.ToString();
+            }
+            else if ((textKelvin.Contains(",") || textKelvin.Contains(".")) && text.Text != "" && kelvin)
+            {
+                this.textBox_Kelvine.Text = degreeKelvin.ToString();
+                textKelvin = degreeKelvin.ToString();
+            }
+            else if(kelvin)
+            {
+                textKelvin = "";
+                degreeKelvin = 0;
             }
         }
 
         private void textBox_Celcia_TextChanged(object sender, EventArgs e)
         {
             var text = sender as TextBox;
-            if (text.Text != "" && celcia)
+            bool right = true;
+            if (text.Text != "" && celcia && !text.Text.StartsWith(".") && !text.Text.StartsWith(",") && (!textCelcia.Contains(",") || (textCelcia.IndexOf(',') == text.Text.LastIndexOf(','))) && (!textCelcia.Contains(".") || (textCelcia.IndexOf('.') == text.Text.LastIndexOf('.'))))
             {
                 if (text.Text != "-")
                 {
                     farinhate = kelvin = false;
-                    double celciaDegree;
                     try
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace('.', ','));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeCelcia = Convert.ToDouble(text.Text.Replace('.', ','));
+                                textCelcia = text.Text;
+                                right = false;
+                            }
+                        }
                     }
                     catch (Exception)
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace(',', '.'));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeCelcia = Convert.ToDouble(text.Text.Replace(',', '.'));
+                                textCelcia = text.Text;
+                                right = false;
+                            }
+                        }
                     }
-                    this.textBox_Farinhate.Text = ConvertCelciaToFarinhate(celciaDegree).ToString();
-                    this.textBox_Kelvine.Text = ConvertCelciaToKelvine(celciaDegree).ToString();
+                    if (right != false)
+                    {
+                        this.textBox_Celcia.Text = degreeCelcia.ToString();
+                        textCelcia = degreeCelcia.ToString();
+                    }
+                    this.textBox_Farinhate.Text = ConvertCelciaToFarinhate(degreeCelcia).ToString();
+                    this.textBox_Kelvine.Text = ConvertCelciaToKelvine(degreeCelcia).ToString();
                     farinhate = kelvin = true;
                 }
+            }
+            else if (text.Text.StartsWith(",") || text.Text.StartsWith("."))
+            {
+                this.textBox_Celcia.Text = degreeCelcia.ToString();
+                textCelcia = degreeCelcia.ToString();
+            }
+            else if ((textCelcia.Contains(",") || textCelcia.Contains(".")) && text.Text != "" && celcia)
+            {
+                this.textBox_Celcia.Text = degreeCelcia.ToString();
+                textCelcia = degreeCelcia.ToString();
+            }
+            else if(celcia)
+            {
+                textCelcia = "";
+                degreeCelcia = 0;
             }
         }
 
         private void textBox_Farinhate_TextChanged(object sender, EventArgs e)
         {
             var text = sender as TextBox;
-            if (text.Text != "" && farinhate)
+            bool right = true;
+            if (text.Text != "" && farinhate && !text.Text.StartsWith(".") && !text.Text.StartsWith(",") && (!textFarinhate.Contains(",") || (textFarinhate.IndexOf(',') == text.Text.LastIndexOf(','))) && (!textFarinhate.Contains(".") || (textFarinhate.IndexOf('.') == text.Text.LastIndexOf('.'))))
             {
                 if (text.Text != "-")
                 {
                     celcia = kelvin = false;
-                    double celciaDegree;
                     try
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace('.', ','));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeFarinhate = Convert.ToDouble(text.Text.Replace('.', ','));
+                                textFarinhate = text.Text;
+                                right = false;
+                            }
+                        }
                     }
                     catch (Exception)
                     {
-                        celciaDegree = Convert.ToDouble(text.Text.Replace(',', '.'));
+                        foreach (char item in charMassive)
+                        {
+                            if (text.Text.EndsWith(item.ToString()))
+                            {
+                                degreeFarinhate = Convert.ToDouble(text.Text.Replace(',', '.'));
+                                textFarinhate = text.Text;
+                                right = false;
+                            }
+                        }
                     }
-                    this.textBox_Kelvine.Text = ConvertFarinhateToKelvine(celciaDegree).ToString();
-                    this.textBox_Celcia.Text = ConvertFarinhateToCelcia(celciaDegree).ToString();
+                    if (right != false)
+                    {
+                        this.textBox_Farinhate.Text = degreeFarinhate.ToString();
+                        textFarinhate = degreeFarinhate.ToString();
+                    }
+                    this.textBox_Kelvine.Text = ConvertFarinhateToKelvine(degreeFarinhate).ToString();
+                    this.textBox_Celcia.Text = ConvertFarinhateToCelcia(degreeFarinhate).ToString();
                     celcia = kelvin = true;
                 }
+            }
+            else if (text.Text.StartsWith(",") || text.Text.StartsWith("."))
+            {
+                this.textBox_Farinhate.Text = degreeFarinhate.ToString();
+                textFarinhate = degreeFarinhate.ToString();
+            }
+            else if ((textFarinhate.Contains(",") || textFarinhate.Contains(".")) && text.Text != "" && farinhate)
+            {
+                this.textBox_Farinhate.Text = degreeFarinhate.ToString();
+                textFarinhate = degreeFarinhate.ToString();
+            }
+            else if(farinhate)
+            {
+                textFarinhate = "";
+                degreeFarinhate = 0;
             }
         }
 

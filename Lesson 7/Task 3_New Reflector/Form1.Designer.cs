a4383702459a938bb0e6ff3be4200fdd7fe26ebf
sergiokredo <sergeykredenser@gmail.com>
2020-08-08 20:42:59 +0300

    Новый интерфейс в приложении рефлектор задания №3.

---
+++ Lesson 7/Task 3_New Reflector/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Task_3_New_Reflector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.Browse = new System.Windows.Forms.Button();
+            this.browser = new System.Windows.Forms.TextBox();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(776, 395);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Browse.Location = new System.Drawing.Point(712, 12);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "Save file...";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // browser
+            // 
+            this.browser.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.browser.Location = new System.Drawing.Point(11, 12);
+            this.browser.Multiline = true;
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(621, 23);
+            this.browser.TabIndex = 3;
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(638, 12);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(68, 23);
+            this.button_Search.TabIndex = 4;
+            this.button_Search.Text = "Open file...";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.button_Search_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.ColumnWidth = 180;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Field",
+            "Constructor",
+            "Property",
+            "Event",
+            "Method",
+            "Interface",
+            "Type attribute information",
+            "Type member attribute information         "});
+            this.checkedListBox1.Location = new System.Drawing.Point(11, 444);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkedListBox1.Size = new System.Drawing.Size(777, 34);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reflector by SergKredo";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.TextBox browser;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Lesson 7/Task 3_New Reflector/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+namespace Task_3_New_Reflector
+{
+    /*
+         Расширьте возможности программы-рефлектора из предыдущего урока следующим образом:
+    1. Добавьте возможность выбирать, какие именно члены типа должны быть показаны
+    пользователю. При этом должна быть возможность выбирать сразу несколько членов
+    типа, например, методы и свойства.
+    2. Добавьте возможность вывода информации об атрибутах для типов и всех членов типа,
+    которые могут быть декорированы атрибутами.
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)   // Метод-обработчик события срабатывает при нажатии на кнопку "Save file..."
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)  // Диалоговое окно для сохранения текущей информации из приложения в текстовый файл
+            {
+                string path = openFileDialog.FileName;   // Путь сохранения
+                File.WriteAllText(@path, this.textBox1.Text, Encoding.UTF8);  // Запись текущей информации из приложения в файл
+            }
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)  // Метод-обработчик события срабатывает при нажатии на кнопку "Open file..."
+        {
+            try
+            {
+                if (openFileDialog.ShowDialog(this) == DialogResult.OK)  // Проверка на условие нажатия пользователем кнопки "OK"в открывшемся диаголовом окне
+                {
+                    this.textBox1.Text = null;
+                    string path = openFileDialog.FileName;  // Строка полного адресного пути загружаемой нами сборки (exe, dll).
+                    this.browser.Text = @path;
+
+
+                    // При помощи класса Assembly - можно динамически загружать сборки, 
+                    // обращаться к членам класса в процессе выполнения (ПОЗДНЕЕ СВЯЗЫВАНИЕ),
+                    // а так же получать информацию о самой сборке.
+
+                    Assembly assembly = Assembly.LoadFrom(@path);
+                    Type[] types = assembly.GetTypes();   // Вывод информации о всех типах в сборке с помощью абстрактного класса Type.
+                    string namespaces = null;
+                    string loadAssembly = new string(' ', 90) + "The assembly was loaded successfully!".ToUpper() + "\r\n\r\n";
+                    this.textBox1.Text += loadAssembly;
+                    foreach (Type item in types)    // Перебор всех типов
+                    {
+                        this.textBox1.Text += (namespaces == null) ? "namespace " + item.Namespace + Environment.NewLine + "{" + Environment.NewLine : null;
+                        namespaces = item.Namespace;
+                        string padding = new string(' ', 5);
+                        if (item.IsClass)  // Проверка, является ли данный тип классом
+                        {
+                            this.textBox1.Text += padding + "class ";
+                        }
+
+                        if (item.IsValueType)  // Проверка, является ли данный тип структурой
+                        {
+                            this.textBox1.Text += padding + "struct ";
+                        }
+
+                        if (item.IsEnum)   // Проверка, является ли данный тип перечислением
+                        {
+                            padding = "";
+                            this.textBox1.Text += padding + "enum ";
+                            padding = new string(' ', 5);
+                        }
+
+                        if (item.IsInterface)  // Проверка, является ли данный тип интерфейсом
+                        {
+                            this.textBox1.Text += padding + "interface ";
+                        }
+
+
+                        this.textBox1.Text += item.Name + Environment.NewLine + padding + "{" + Environment.NewLine;
+
+                        padding = new string(' ', 10);
+                        this.textBox1.Text += padding + "Methods:".ToUpper() + Environment.NewLine;
+
+
+                        /*Перечисление BindingFlags может принимать различные значения:
+                            DeclaredOnly: получает только методы непосредственно данного класса, унаследованные методы не извлекаются
+                            Instance: получает только методы экземпляра
+                            NonPublic: извлекает не публичные методы
+                            Public: получает только публичные методы
+                            Static: получает только статические методы
+                        */
+                        foreach (MethodInfo items in item.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.IgnoreCase))  // Перебор всех методов
+                        {
+                            bool enter = true;
+                            padding = new string(' ', 42);
+                            if (items.IsPublic)   // Проверка, является ли данный метод открытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "public ";
+                            }
+
+                            if (items.IsPrivate)  // Проверка, является ли данный метод закрытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "private ";
+                            }
+
+                            if (items.IsStatic)  // Проверка, является ли данный метод статическим
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "static ";
+                            }
+
+                            if (items.IsAbstract)  // Проверка, является ли данный метод абстрактным
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "abstract ";
+                            }
+
+                            if (items.IsVirtual)  // Проверка, является ли данный метод виртуальным
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "virtual ";
+                            }
+                            if (enter)
+                            {
+                                this.textBox1.Text += padding;
+                            }
+
+                            this.textBox1.Text += items.ReturnType.Name + " " + items.Name + "(";
+
+
+                            ParameterInfo[] parameters = items.GetParameters();  // Определение параметров заданного метода
+                            string text = null;
+                            for (int i = 0; i < parameters.Length; i++)
+                            {
+                                this.textBox1.Text += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                                text += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                                if (i + 1 < parameters.Length)
+                                {
+                                    this.textBox1.Text += ", ";
+                                    text += ", ";
+                                }
+                            }
+                            this.textBox1.Text += ");" + Environment.NewLine;
+                        }
+
+
+                        padding = new string(' ', 10);
+                        this.textBox1.Text += padding + "Fields:".ToUpper() + Environment.NewLine;
+
+                        // Перебор всех полей типа
+                        foreach (var field in item.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.IgnoreCase))
+                        {
+                            bool enter = true;
+                            padding = new string(' ', 42);
+                            if (field.IsPublic)  // Проверка, является ли данное поле открытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "public ";
+                            }
+
+                            if (field.IsPrivate)  // Проверка, является ли данное поле закрытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "private ";
+                            }
+
+                            if (field.IsStatic)  // Проверка, является ли данное поле статическим
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "static ";
+                            }
+                            if (enter)
+                            {
+                                this.textBox1.Text += padding;
+                            }
+                            this.textBox1.Text += field.FieldType.Name + " " + field.Name + ";" + Environment.NewLine;
+                        }
+
+                        // Перебор всех свойств типа
+                        padding = new string(' ', 10);
+                        this.textBox1.Text += padding + "Properties:".ToUpper() + Environment.NewLine;
+                        foreach (var field in item.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.IgnoreCase))
+                        {
+                            bool enter = true;
+                            padding = new string(' ', 42);
+                            this.textBox1.Text += padding;
+
+                            this.textBox1.Text += field.PropertyType.Name + " " + field.Name + ";" + Environment.NewLine;
+                        }
+
+                        // Перебор всех конструкторов типа
+                        padding = new string(' ', 10);
+                        this.textBox1.Text += padding + "Constructors:".ToUpper() + Environment.NewLine;
+                        foreach (var field in item.GetConstructors(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.IgnoreCase))
+                        {
+                            bool enter = true;
+                            padding = new string(' ', 42);
+                            if (field.IsPublic)  // Проверка, является ли данный конструктор открытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "public ";
+                            }
+
+                            if (field.IsPrivate)  // Проверка, является ли данный конструктор закрытым
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "private ";
+                            }
+
+                            if (field.IsStatic)  // Проверка, является ли данный конструктор статическим
+                            {
+                                padding = enter ? padding : "";
+                                enter = false;
+                                this.textBox1.Text += padding + "static ";
+                            }
+
+                            if (enter)
+                            {
+                                this.textBox1.Text += padding;
+                            }
+
+                            this.textBox1.Text += field.Name + "(";
+
+                            // Определение параметров заданного конструктора
+                            ParameterInfo[] parameters = field.GetParameters();
+                            string text = null;
+                            for (int i = 0; i < parameters.Length; i++)
+                            {
+                                this.textBox1.Text += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                                text += parameters[i].ParameterType.Name + " " + parameters[i].Name;
+                                if (i + 1 < parameters.Length)
+                                {
+                                    this.textBox1.Text += ", ";
+                                    text += ", ";
+                                }
+                            }
+                            this.textBox1.Text += ");" + Environment.NewLine;
+                        }
+
+
+                        padding = new string(' ', 5);
+                        this.textBox1.Text += padding + "}" + Environment.NewLine + "\r\n";
+
+                    }
+                    this.textBox1.Text += "}" + Environment.NewLine;
+                }
+            }
+            catch (Exception)  // Исключение срабатывает в случае ошибки при открытии иной сборки, которая не потдерживается платформой .Net framework
+            {
+                string loadAssembly = new string(' ', 90) + "Failed assembly loading!".ToUpper();
+                this.textBox1.Text = loadAssembly;
+            }
+        }
+    }
+}

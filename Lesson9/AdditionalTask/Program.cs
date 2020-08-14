6ecec0ac1d372e215070724717fb3ed564eb4a54
sergiokredo <sergeykredenser@gmail.com>
2020-08-14 23:43:08 +0300

    Приложение №1.

---
+++ Lesson9/AdditionalTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    /*
+         Создайте свой класс, объекты которого будут занимать много места в памяти (например, в
+    коде класса будет присутствовать большой массив) и реализуйте для этого класса,
+    формализованный шаблон очистки.
+     */
+    public class MyClass : IDisposable
+    {
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

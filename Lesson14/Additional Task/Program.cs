bc19921f3c8e116451b5579cdb4dd95112983f58
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-09-16 11:58:33 +0300

    Создание консольных приложений для работы с TPL библиотекой

---
+++ Lesson14/Additional Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Task
+{
+        /*Создайте массив чисел размерностью в 1 000 000 или более. Используя генератор случайных
+    чисел, проинициализируйте этот массив значениями. Создайте PLINQ запрос, который
+    позволит получить все нечетные числа из исходного массива.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lesson14/Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+        /*Создайте два метода, которые будут выполняться в рамках параллельных задач. Организуйте
+    вызов этих методов при помощи Invoke таким образом, чтобы основной поток программы
+    (метод Main) не остановил свое выполнение.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

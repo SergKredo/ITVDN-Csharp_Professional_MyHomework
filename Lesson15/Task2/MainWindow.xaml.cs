124e2cd40c6260983455cf831f7c812a4b0424fb
sergiokredo <sergeykredenser@gmail.com>
2020-09-26 21:40:08 +0300

    Рабочая версия приложения Task2 с комментариями в коде программы

--- Lesson15/Task2/MainWindow.xaml.cs
+++ Lesson15/Task2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Task2
 {
@@ -30,9 +31,55 @@ namespace Task2
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Forms.Timer timer; // Добавляем ссылку на сборку System.Windows.Forms и объявляем объект класса Timer
         public MainWindow()
         {
             InitializeComponent();
+            this.timer = new System.Windows.Forms.Timer(); // Создаем экземпляр класса Timer
+            this.timer.Interval = 5000;  // Задаем интервал между последним и настоящим вызовом события Tick экземпляра класса Timer
+            this.timer.Tick += Timer_Tick;  // Сообщаем метод обработчик с событием Tick
+            this.BottonDisconnect.IsEnabled = false;  // Делаем неактивной кнопку Disconnect
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)  // Метод обработчик события Tick
+        {
+            this.TextData.Text += "Data received" + Environment.NewLine;
+        }
+
+        private async void BottonConnect_Click(object sender, RoutedEventArgs e) // Асинхронный вызов метода обработчика при нажатии на кнопку Connect
+        {
+            this.TextData.Text += await ConnectToBaseData(); // Присваиваем возвращаемое значение типа string при асинхронном вызове метода ConnectToBaseData()
+            this.BottonConnect.IsEnabled = false;
+            this.BottonDisconnect.IsEnabled = true;
+            this.timer.Start();  // Запускаем таймер c последовательностью повторяющихся периодичных событий Tick
+        }
+
+        private async void BottonDisconnect_Click(object sender, RoutedEventArgs e)  // Асинхронный вызов метода обработчика при нажатии на кнопку Disconnect
+        {
+            this.TextData.Text += await DisconnectToBaseData(); // Присваиваем возвращаемое значение типа string при асинхронном вызове метода DisconnectToBaseData()
+            this.BottonConnect.IsEnabled = true;  // Делаем активной кнопку
+            this.timer.Stop();  // Останавливаем таймер
+            this.BottonDisconnect.IsEnabled = false;
+        }
+
+        async Task<string> ConnectToBaseData()  // Асинхронный метод, который возвращает задачу generics Task типа string
+        {
+            return await Task.Run(() => // Класс-объект Task ставит в очередь заданную работу для запуска в пуле потоков и возвращает объект типа Task<TResult>
+            {
+                Thread.Sleep(5000);  // Приостанавливаем работу ьекущего потока на 5 миллисекунд
+                return "Connected to database" + Environment.NewLine;
+            }
+            );
+        }
+
+        async Task<string> DisconnectToBaseData()  // Асинхронный метод, который возвращает задачу generics Task типа string
+        {
+            return await Task.Run(() =>  // Класс-объект Task ставит в очередь заданную работу для запуска в пуле потоков и возвращает объект типа Task<TResult>
+            {
+                Thread.Sleep(5000);
+                return "Disconnected to database" + Environment.NewLine;
+            }
+            );
         }
     }
 }
